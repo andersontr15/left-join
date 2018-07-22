72aa18863fabe1ba82fa156081cc0dcd60596d12
Theodore Rollin Anderson <theoanderson3@gmail.com>
2018-07-21 22:25:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
   July 21 2018
 */
 
-const left_join = (table1, table2, key) => {
+const left_join = (table1 = [], table2 = [], key = '') => {
   return table1.map(table1Row => {
     const matchInTableTwo = table2.find(
       table2Row => table2Row.id === table1Row[key]
