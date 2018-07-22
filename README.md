02278fc8933b622a6debe0e3116236d319ddcef8
Theo Anderson <theoanderson3@gmail.com>
2018-07-21 20:42:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Simple Left Join in JavaScript
---
+++ index.js
@@ -0,0 +1,20 @@
+/* Simple left join in JavaScript 
+  Written by Theodore Anderson
+  July 21 2018
+*/
+
+const left_join = (table1, table2, key) => {
+  return table1.map(table1Row => {
+    const matchInTableTwo = table2.find(
+      table2Row => table2Row[key] === table1Row[key]
+    );
+    if (matchInTableTwo) {
+      return {
+        ...table1Row,
+        ...matchInTableTwo
+      };
+    } else {
+      return table1Row;
+    }
+  });
+};
